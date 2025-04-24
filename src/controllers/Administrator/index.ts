c6e537e3b9b18afe718fffdd7e521dc80446a97a
Jeremy <jeremyramos150@gmail.com>
2025-04-23 20:17:19 -0600

    Cambios backend

--- src/controllers/Administrator/index.ts
+++ src/controllers/Administrator/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import * as AdministratorModel from '../../models/Administrator/index';
+import * as AdministratorModel from '../../models/Administrator/index.js';
 export const getAllAdministrators = async (req: Request, res: Response) => {
   try {
     const administrators = await AdministratorModel.getAdministrators();
